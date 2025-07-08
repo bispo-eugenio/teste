d72147aac275cbee1b69df8a3b51453944a28c5d
bispo-eugenio <bispoeugenio270@gmail.com>
2025-07-08 11:54:58 -0300

    initial

---
+++ main.js
@@ -0,0 +1,3 @@
+window.addEventListener("mousemove", (e)=> { /*Busca evento de mexer o mouse da página*/
+setTimeout(() => document.getElementById("container-main__audio").play(), 1000) /*Espera de 1s*/
+})
