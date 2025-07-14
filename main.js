73236efba7be59c8e879964e3fc5271875f11795
bispo-eugenio <bispoeugenio270@gmail.com>
2025-07-14 05:44:06 -0300

    doc: reorganiza arquivos.

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 window.addEventListener("mousemove", (e)=> { /*Busca evento de mexer o mouse da página*/
 setTimeout(() => document.getElementById("container-main__audio").play(), 1000) /*Espera de 1s*/
 })
+
+
+function submitValue(){
+    document.querySelector("button").addEventListener( "click", function() {
+        let value = document.querySelector(".main-challenger__info--input").value
+    })
+}
+const question = [
+    "*Qual a distância percorrida da sua casa até seu trabalho (em km)"
+]
+let distance = 0;
+let container = document.querySelector(".main-challenger__box-img--dialog-cloud-text")
+while(true){
+    do {
+        paragraph = document.createElement("p")
+    } while(isNaN(paragraph))
+    break;
+}
